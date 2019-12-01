ab33c1ba35740ba1868a98611c58d5cee02bc495
Thiago Dantas <thiagodma52@gmail.com>
2019-12-01 15:25:04 -0300

    now it links correctly

--- LM_csv_File_Creation.py
+++ LM_csv_File_Creation.py
@@ -4,7 +4,7 @@ import PyPDF2
 
 df = pd.read_csv("scraping_filtered.csv")
 
-pdf_list = os.listdir("Datafiltered/")
+pdf_list = list(df['Filename'])
 
 def getText2PDF(pdfFileName):
     pdf_file=open("Datafiltered/" + pdfFileName,'rb')
@@ -13,32 +13,14 @@ def getText2PDF(pdfFileName):
     for i in range(0,read_pdf.getNumPages()):
         text.append(read_pdf.getPage(i).extractText())
     return ('\n'.join (text).replace("\n",''))
-    
-    
+
 list_txt = []
-lista_erros = []
 for i in range(len(pdf_list)):
     try:
       list_txt.append(getText2PDF(pdf_list[i]))
     except:
       print("Erro na conversão do arquivo " + pdf_list[i])
-      lista_erros.append(pdf_list[i])
+      list_txt.append('Arquivo corrompido.')
 
-newpdf_list = [x for x in pdf_list if x not in lista_erros]
-listabool = []
-for i in range(len(df)):
-  listabool.append(df.iloc[i, 7] in newpdf_list)
-dfbool = pd.DataFrame(listabool)
-df["exists"] = dfbool
-df = df[df["exists"] == True]
-
-del df["exists"]
-
-dflist = pd.DataFrame(list_txt)
-df["Texto Extraído"] = dflist
+df['Texto Extraído'] = pd.Series(list_txt)
 df.to_csv("LanguageModelFile.csv", sep = "|")
-
-verif = pd.read_csv("LanguageModelFile.csv", sep = "|")
-
-print(verif.shape)
-
